9f83dfdaf4cbd6c32dfdc9be5b70b18dc1aa673a
Firexit <workwithpurpose@yahoo.com>
2018-02-11 23:48:23 -0600

    Initial commit

---
+++ app/src/main/java/com/example/anna/partyparrot/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.anna.partyparrot;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+    private ImageView img;
+
+    @SuppressLint("NewApi") @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img = (ImageView) findViewById(R.id.img);
+
+        img.post(new Runnable() {
+
+            @Override
+            public void run() {
+                ((AnimationDrawable) img.getBackground()).start();
+            }
+
+        });
+    }
+}
